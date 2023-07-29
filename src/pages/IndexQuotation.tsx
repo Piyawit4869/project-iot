9351ed73e2cdf87fb563435655a79cd423c8536b
OverloadsOP <xdireksmmana@gmail.com>
2023-07-29 17:09:31 +0700

    style(theme_style): add theme and style in index quotation page

--- src/pages/IndexQuotation.tsx
+++ src/pages/IndexQuotation.tsx
@@ -1,12 +1,10 @@
-import { Button, Card, Select, Table, } from "antd";
-import { Link } from "react-router-dom";
-import { PlusOutlined } from "@ant-design/icons";
-
+import { Button, Dropdown, Form, MenuProps, Row, Space, Table, Typography, message, } from "antd";
+import { Title } from "../components/global/Title";
+import { ThemeColors } from "../styles/theme";
+import { DownOutlined, PlusSquareOutlined } from "@ant-design/icons";
+import Column from "antd/es/table/Column";
 
 export const IndexQuotationPage = () => {
-    const handleChange = (_value: string[]) => {
-        console.log('selected ${value}');
-    }
 
     const dataSource = [
         {
@@ -16,6 +14,42 @@ export const IndexQuotationPage = () => {
           issueDate: '01/01/2566',
           validDate: '01/01/2566',
           value: 2000,
+          status: 'ร่าง',
+        },
+        {
+          key: '2',
+          documentNumber: 1235,
+          name: 'Po',
+          issueDate: '01/01/2566',
+          validDate: '01/01/2566',
+          value: 212100,
+          status: 'ร่าง',
+        },
+        {
+          key: '2',
+          documentNumber: 1235,
+          name: 'Po',
+          issueDate: '01/01/2566',
+          validDate: '01/01/2566',
+          value: 212100,
+          status: 'ร่าง',
+        },
+        {
+          key: '2',
+          documentNumber: 1235,
+          name: 'Po',
+          issueDate: '01/01/2566',
+          validDate: '01/01/2566',
+          value: 212100,
+          status: 'ร่าง',
+        },
+        {
+          key: '2',
+          documentNumber: 1235,
+          name: 'Po',
+          issueDate: '01/01/2566',
+          validDate: '01/01/2566',
+          value: 212100,
           status: 'ร่าง',
         },
         {
@@ -62,40 +96,107 @@ export const IndexQuotationPage = () => {
         },
       ];
 
+      const itemsStatus: MenuProps["items"] = [
+    {
+      label: "ทั้งหมด",
+      key: "1",
+    },
+    {
+      label: "ร่าง",
+      key: "2",
+    },
+    {
+      label: "รออนุมัติ",
+      key: "3",
+    },
+    {
+      label: "รอตอบรับ",
+      key: "4",
+    },
+    {
+      label: "ยอมรับแล้ว",
+      key: "5",
+    },
+  ];
+
+  const onClickStatus: MenuProps["onClick"] = ({ key }) => {
+    message.info(`Click on item ${key}`);
+  };
+
   return (
-
-        <Card
-           title="quotation"
-           extra={<Link to="/dashboard">เพิ่มเติม</Link>}>
         <div>
-          <div className="status">
-              <Select
-                className="status_select"
-                style={{minWidth:'175px',}}
-                defaultValue={['ทั้งหมด']}
-                onChange={handleChange}
-                options={[
-                  {value: 'ทั้งหมด', label:'ทั้งหมด'},
-                  {value: 'ร่าง', label:'ร่าง'},
-                  {value: 'รออนุมัติ', label:'รออนุมัติ'},
-                  {value: 'รอตอบรับ', label:'รอตอบรับ'},
-                  {value: 'ตอบรับแล้ว', label:'ตอบรับแล้ว'}
-                ]}>
-              </Select>
+          {Title("ใบเสนอราคา")}
+          <Row>
+           <div
+            style={{
+              height:"75px",
+              width: "20%",
+              backgroundColor: "white",
+              paddingTop: "10px",
+              paddingLeft: "10px",
+              borderRadius: "10px",
+              boxShadow: "2px 2px 2px 3px" + ThemeColors.goldColor,
+              marginRight: "20px",
+            }}
+          >
+            <Typography style={{ fontSize: "18px" }}>สถานะ</Typography>
+            <Form.Item>
+              <Dropdown
+                menu={{ items: itemsStatus, onClick: onClickStatus }}
+              >
+                <a onClick={(e) => e.preventDefault()}>
+                  <Space
+                    style={{
+                      width: "90%",
+                      display: "flex",
+                      justifyContent: "space-between",
+                      color: ThemeColors.lightOrangeColor,
+                    }}
+                  >
+                    -
+                    <DownOutlined />
+                  </Space>
+                </a>
+              </Dropdown>
+            </Form.Item>
           </div>
-          <div className="add_new_quotation">
-            <Link to="/quotation">
-              <Button type="primary" icon={<PlusOutlined />} 
-              style={{width:'175px', height:'75px'}} >
-                    สร้างใบเสนอราคา
-              </Button>
-            </Link>
+          <div>
+            <Button
+              style={{ height: "75px", width:'265px', backgroundColor: ThemeColors.orangeColor }}
+            >
+              <Typography style={{ fontSize: "18px", color: "white" }}>
+                <PlusSquareOutlined style={{
+                color: ThemeColors.whiteColor,
+                fontSize: "24px",
+                marginRight: "5px",
+              }}/>
+                อนุมัติในเสนอราคา
+              </Typography>
+            </Button>
           </div>
+          </Row>
+        <div
+          style={{
+          width:'100%',
+          height:'124px',
+          flexShrink:'0', 
+          borderRadius:'10px', 
+          borderBottom:'1px solid var(--palettes-22, #EE9437', 
+          background:'var(--palettes-22, #EE9437)',
+          marginTop:'20px',
+          }}>
+            <Table columns={columns} dataSource={dataSource}></Table>
         </div>
-        <div className="table_index_quota">
-            <Table columns={columns} dataSource={dataSource} style={{marginTop:'20px'}}/>
+        <div 
+        style={{
+          minWidth:'100vh',
+          minHeight:'100vh',
+          flexShrink:'0', 
+          borderRadius:'10px', 
+          border:'3px solid var(--palettes-22, #EE9437', 
+          background:'var(--box-color, #FFF)', 
+          }}>
         </div>
-        </Card>
-
+      </div>
   );
 };
