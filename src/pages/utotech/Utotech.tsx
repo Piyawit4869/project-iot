7e3a6e8079088d66a36aedf93ef6bda9c958512d
phuwis <phuwis.tweed@gmail.com>
2023-11-20 19:10:29 +0700

    fix(parallax): add parallax layout and fixed content

--- src/pages/utotech/Utotech.tsx
+++ src/pages/utotech/Utotech.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, Col, Image, Row, Typography } from "antd";
 import React from "react";
+import { Parallax } from "react-parallax";
 import { Link } from "react-router-dom";
 
 import { TypeAnimation } from "react-type-animation";
@@ -18,10 +19,37 @@ const bannerTexts = [
 ];
 // Mock data
 const mockData = [
-  { id: 1, title: "Item 1", description: "Description for Item 1" },
-  { id: 2, title: "Item 2", description: "Description for Item 2" },
-  { id: 3, title: "Item 3", description: "Description for Item 3" },
-  { id: 4, title: "Item 4", description: "Description for Item 4" },
+  {
+    id: 1,
+    title: "Website",
+    description: "Description for Item 1",
+    image: "https://cdn-icons-png.flaticon.com/512/6471/6471842.png",
+  },
+  {
+    id: 2,
+    title: "Web Application",
+    description: "Description for Item 2",
+    image: "https://cdn-icons-png.flaticon.com/512/5447/5447882.png",
+  },
+  {
+    id: 3,
+    title: "Mobile Application",
+    description: "Description for Item 3",
+    image: "https://cdn-icons-png.flaticon.com/512/6213/6213889.png",
+  },
+  {
+    id: 4,
+    title: "Android and IOS",
+    description: "Description for Item 4",
+    image:
+      "https://cdn0.iconfinder.com/data/icons/influencer-marketing-wildberry-volume-1/256/Cross_Platform-512.png",
+  },
+  {
+    id: 5,
+    title: "IOT",
+    description: "Description for Item 5",
+    image: "https://cdn-icons-png.flaticon.com/512/6091/6091352.png",
+  },
 ];
 
 export const Utotech: React.FC = () => {
@@ -39,34 +67,50 @@ export const Utotech: React.FC = () => {
 
   return (
     <div>
-      <div
+      <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={
+          "https://4kwallpapers.com/images/wallpapers/dark-background-abstract-background-network-3d-background-6016x3384-8324.png"
+        }
+        strength={200}
+        bgImageStyle={{ objectFit: "cover" }}
+        bgImageAlt="the dog"
         style={{
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
           justifyContent: "center",
-          backgroundColor: "#cccccc",
           width: "100%",
-          height: "80vh",
           marginBottom: "120px",
         }}
       >
-        <Typography
+        <div
           style={{
-            fontSize: "36px",
-            marginBottom: "20px",
-            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "80vh",
           }}
         >
-          Welcome to UTOTECH COMPANY LIMITED
-        </Typography>
-        <TypeAnimation
-          sequence={bannerTexts}
-          speed={50}
-          repeat={Infinity}
-          style={{ fontSize: "28px", color: "grey", textAlign: "center" }}
-        />
-      </div>
+          <Typography
+            style={{
+              fontSize: "36px",
+              marginBottom: "20px",
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            Welcome to UTOTECH COMPANY LIMITED
+          </Typography>
+          <TypeAnimation
+            sequence={bannerTexts}
+            speed={50}
+            repeat={Infinity}
+            style={{ fontSize: "28px", color: "white", textAlign: "center" }}
+          />
+        </div>
+      </Parallax>
+
       <div
         style={{
           paddingLeft: "10%",
@@ -74,57 +118,48 @@ export const Utotech: React.FC = () => {
           marginBottom: "120px",
         }}
       >
-        {/* <div style={{ width: "60%" }}>
-          <Typography style={{ fontSize: "36px" }}>
-            Welcome to UTOTECH COMPANY LIMITED
-          </Typography>
-        </div>
-
-        <Row
-          justify="center"
-          style={{
-            paddingTop: "30px",
-            paddingLeft: "25%",
-            paddingRight: "25%",
-            paddingBottom: "60px",
-          }}
-        >
-          <Typography style={{ fontSize: "20px", color: "grey" }}>
-            Lorem Ipsum has been the industry's standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
-          </Typography>
-        </Row> */}
         <Typography style={{ fontSize: "28px", marginBottom: "10px" }}>
           What we can do
         </Typography>
-        <Typography style={{ fontSize: "18px", color: "grey" }}>
+        <Typography
+          style={{ fontSize: "18px", color: "grey", marginBottom: "20px" }}
+        >
           I'd be happy to help once I have a better understanding of your
           request.
         </Typography>
-        <Row gutter={16}>
+        <Row gutter={[16, 16]}>
           {mockData.map((item, id) => (
-            <Col key={item.id} span={12} md={12} lg={12} xl={12}>
+            <Col key={item.id} span={24} md={12} lg={12} xl={12}>
               <Link to={`option/${id}`}>
-                <Card>
-                  <Image
-                    preview={false}
-                    src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"
-                  />
+                <Card style={{ backgroundColor: "#aaf0d1" }}>
+                  <Row justify="center">
+                    <div style={{ width: "50%", objectFit: "cover" }}>
+                      <Image preview={false} src={item.image} />
+                    </div>
+                  </Row>
 
                   <h2>{item.title}</h2>
-                  <p>{item.description}</p>
+                  <div style={{ height: "100px" }}>
+                    <p>{item.description}</p>
+                  </div>
                 </Card>
               </Link>
             </Col>
           ))}
         </Row>
       </div>
-      <Image
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOxgXTO4Kc4XORUFvZembSzymC7B6RYupJLQ&usqp=CAU"
-        width="100%"
-        preview={false}
-      />
+      <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={
+          "https://i2.wp.com/catesthill.com/wp-content/uploads/2019/12/catesthill-shoreditch-loft-kasia-fiszer-32.jpg?fit=2400%2C1600&ssl=1"
+        }
+        strength={200}
+        bgImageStyle={{ objectFit: "cover" }}
+        style={{ width: "100%" }}
+      >
+        <div style={{ height: "80vh" }} />
+      </Parallax>
+
       <div
         style={{
           paddingLeft: "10%",
@@ -146,30 +181,42 @@ export const Utotech: React.FC = () => {
         </Row>
       </div>
 
-      <div
+      <Parallax
+        bgImage="https://picjumbo.com/wp-content/uploads/black-styled-minimal-office-things-room-for-text-free-photo.jpg"
+        strength={300}
+        blur={{ min: -15, max: 15 }}
+        bgImageStyle={{ objectFit: "cover" }}
         style={{
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
           justifyContent: "center",
-          backgroundColor: "#cccccc",
           width: "100%",
-          height: "50vh",
         }}
       >
-        <Typography
+        <div
           style={{
-            fontSize: "20px",
-            marginBottom: "20px",
-            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "60vh",
           }}
         >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </Typography>
-        <Button style={{ ...buttonStyle, ...buttonHoverStyle }}>
-          Let's chat
-        </Button>
-      </div>
+          <Typography
+            style={{
+              fontSize: "20px",
+              marginBottom: "20px",
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Typography>
+          <Button style={{ ...buttonStyle, ...buttonHoverStyle }}>
+            Let's chat
+          </Button>
+        </div>
+      </Parallax>
     </div>
   );
 };
