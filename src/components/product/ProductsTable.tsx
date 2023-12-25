52152c5aa18f4bc864fb4635cd6dffb8453eac70
PatrickX14 <kitjapong123@gmail.com>
2023-12-25 21:45:01 +0700

    fix: 🐛 Dashboard
    change component in product page

--- src/components/product/ProductsTable.tsx
+++ src/components/product/ProductsTable.tsx
@@ -1,113 +1,76 @@
-// import Table from "@src/components/global/Table";
-// import { ThemeColors } from "@src/styles/theme";
-// import { ColumnsType } from "antd/es/table";
+import { Button, Row, Table } from "antd";
+import { ColumnsType } from "antd/es/table";
+import { Link, useLoaderData } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { ThemeColors } from "@src/styles/theme";
 
-// export default function ProductTable() {
-// 	const columns: ColumnsType<Datatype> = [
-// 		{
-// 			title: "",
-// 			dataIndex: "key",
-// 			render: () => (
-// 				<EyeOutlined
-// 					style={{ fontSize: "26px", color: ThemeColors.grayColor }}
-// 				/>
-// 			),
-// 		},
-// 		{
-// 			title: t("first name"),
-// 			dataIndex: "fname",
-// 			key: "fname",
-// 			render: (fname: String) => (
-// 				<p style={{ color: ThemeColors.goldColor }}>{fname}</p>
-// 			),
-// 			responsive: ["md"],
-// 		},
-// 		{
-// 			title: t("last name"),
-// 			dataIndex: "lname",
-// 			key: "lname",
-// 			render: (lname: String) => (
-// 				<p style={{ color: ThemeColors.goldColor }}>{lname}</p>
-// 			),
-// 		},
-// 		{
-// 			title: t("role"),
-// 			dataIndex: "role",
-// 			key: "role",
-// 			render: (role: String) => (
-// 				<p style={{ color: ThemeColors.blackColor }}>{role}</p>
-// 			),
-// 		},
-// 		{
-// 			title: t("phone number"),
-// 			dataIndex: "phone",
-// 			key: "phone",
-// 			render: (role: String) => (
-// 				<p style={{ color: ThemeColors.grayColor }}>{role}</p>
-// 			),
-// 		},
-// 		{
-// 			title: t("email"),
-// 			dataIndex: "email",
-// 			key: "email",
-// 			responsive: ["md", "lg"],
-// 			render: (email: String) => (
-// 				<p
-// 					style={{
-// 						color: ThemeColors.grayColor,
-// 					}}
-// 				>
-// 					{email}
-// 				</p>
-// 			),
-// 		},
-// 		{
-// 			title: t("created at"),
-// 			dataIndex: "createdAt",
-// 			key: "createdAt",
-// 			render: (createDate: String) => (
-// 				<p style={{ color: ThemeColors.grayColor }}>{createDate}</p>
-// 			),
-// 		},
-// 		{
-// 			title: t("last used"),
-// 			dataIndex: "lastActive",
-// 			key: "lastActive",
-// 			render: (lastActive: String) => (
-// 				<p style={{ color: ThemeColors.grayColor }}>{lastActive}</p>
-// 			),
-// 		},
-// 		{
-// 			title: t("status"),
-// 			dataIndex: "status",
-// 			key: "status",
-// 			render: (status: String) =>
-// 				status === "Active" ? (
-// 					<p style={{ color: "#22BE7D" }}>{status}</p>
-// 				) : status === "Inactive" ? (
-// 					<p style={{ color: "#EA5959" }}>{status}</p>
-// 				) : (
-// 					<p style={{ color: "#F0BA3D" }}>{status}</p>
-// 				),
-// 		},
-// 		{
-// 			title: "Action",
-// 			dataIndex: "id",
-// 			key: "id",
-// 			render: (id: any) => (
-// 				<Button>
-// 					<Link to={`/users/${id}`}>View</Link>
-// 				</Button>
-// 			),
-// 		},
-// 	];
+interface Datatype {
+	key: String;
+	productname: String;
+	description: String;
+	type: String;
+	count: number;
+	price: String;
 
-// 	return (
-// 		<Table
-// 			size="middle"
-// 			columns={columns}
-// 			dataSource={props.data}
-// 			scroll={{ x: "max-content" }}
-// 		/>
-// 	);
-// }
+	// status: any;
+}
+
+export const ProductTable = () => {
+	const { t } = useTranslation();
+	const { product } = useLoaderData() as any;
+	const columns: ColumnsType<Datatype> = [
+		{
+			title: t("product and service name"),
+			dataIndex: "title",
+			key: "title",
+			render: (productname: String) => (
+				<p style={{ color: ThemeColors.orangeColor }}>{productname}</p>
+			),
+		},
+		{
+			title: t("description"),
+			dataIndex: "description",
+			key: "description",
+			render: (description: String) => (
+				<p style={{ color: ThemeColors.blackColor }}>{description}</p>
+			),
+		},
+		{
+			title: t("quantity"),
+			dataIndex: "quantity",
+			key: "quantity",
+			render: (quantity: String) => (
+				<p style={{ color: ThemeColors.blackColor }}>{quantity}</p>
+			),
+		},
+		{
+			title: t("price"),
+			dataIndex: "price",
+			key: "price",
+			render: (price: number) => (
+				<p style={{ color: ThemeColors.blackColor }}>{price}</p>
+			),
+		},
+		{
+			title: "Action",
+			dataIndex: "title",
+			key: "action",
+			render: (title: any) => (
+				<Button>
+					<Link to={`/products/${title}`}>View</Link>
+				</Button>
+			),
+		},
+	];
+	return (
+		<Row>
+			<Table
+				size="middle"
+				pagination={false}
+				columns={columns}
+				dataSource={product}
+				style={{ marginTop: "15px", textAlign: "center", width: "100%" }}
+			/>
+		</Row>
+	);
+};
--- src/pages/products/ProductIndex.tsx
+++ src/pages/products/ProductIndex.tsx
@@ -1,40 +1,15 @@
-import { ThemeColors } from "@src/styles/theme";
 import { Title } from "@src/components/global/Title";
-import * as API from "@src/apis";
-// import ProductTable from "@src/components/product/ProductsTable";
-import {
-	DownOutlined,
-	PlusOutlined,
-	SearchOutlined,
-	ControlOutlined,
-} from "@ant-design/icons";
-import {
-	Input,
-	Button,
-	Dropdown,
-	Space,
-	MenuProps,
-	Typography,
-	Form,
-	Table,
-	Row,
-	message,
-} from "antd";
+
+import { MenuProps, Typography, message } from "antd";
 import { useTranslation } from "react-i18next";
-import { ColumnsType } from "antd/es/table";
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+
+import { SearchBox } from "@src/components/global/SearchBox";
+import { FilterBar } from "@src/components/product/FilterBar";
+import { ProductTable } from "@src/components/product/ProductsTable";
+import * as API from "@src/apis";
+import { ControlOutlined } from "@ant-design/icons";
 import { useState } from "react";
-
-interface Datatype {
-	key: String;
-	productname: String;
-	description: String;
-	type: String;
-	count: number;
-	price: String;
-
-	// status: any;
-}
 
 export async function productIndexLoader() {
 	// const url = new URL(params.request.url);
@@ -56,76 +31,7 @@ export const ProductPage = () => {
 	const { product } = useLoaderData() as any;
 	console.log(product);
 
-	const columns: ColumnsType<Datatype> = [
-		// {
-		// 	title: "",
-		// 	dataIndex: "key",
-		// 	render: (key: String) => (
-		// 		<p style={{ color: ThemeColors.orangeColor }}>{key}</p>
-		// 	),
-		// },
-		{
-			title: t("product and service name"),
-			dataIndex: "title",
-			key: "title",
-			render: (productname: String) => (
-				<p style={{ color: ThemeColors.orangeColor }}>{productname}</p>
-			),
-		},
-		{
-			title: t("description"),
-			dataIndex: "description",
-			key: "description",
-			render: (description: String) => (
-				<p style={{ color: ThemeColors.blackColor }}>{description}</p>
-			),
-		},
-		{
-			title: t("quantity"),
-			dataIndex: "quantity",
-			key: "quantity",
-			render: (quantity: String) => (
-				<p style={{ color: ThemeColors.blackColor }}>{quantity}</p>
-			),
-		},
-		{
-			title: t("price"),
-			dataIndex: "price",
-			key: "price",
-			render: (price: number) => (
-				<p style={{ color: ThemeColors.blackColor }}>{price}</p>
-			),
-		},
-		{
-			title: "Action",
-			dataIndex: "title",
-			key: "action",
-			render: (title: any) => (
-				<Button>
-					<Link to={`/products/${title}`}>View</Link>
-				</Button>
-			),
-		},
-		// {
-		// 	render: (key: any) => (
-		// 		<Link to={`/admin/product/update`}>
-		// 			<Button
-		// 				onClick={() => console.log(key.key)}
-		// 				style={{
-		// 					backgroundColor: ThemeColors.orangeColor,
-		// 					color: ThemeColors.whiteColor,
-		// 					border: "0px",
-		// 					height: "40px",
-		// 					width: "100px",
-		// 				}}
-		// 				icon={<EditOutlined style={{ fontSize: "18px" }} />}
-		// 			>
-		// 				แก้ไข
-		// 			</Button>
-		// 		</Link>
-		// 	),
-		// },
-	];
+	const [collapsed, setCollapsed] = useState(false);
 
 	const itemsCurrency: MenuProps["items"] | any = [
 		{
@@ -138,31 +44,6 @@ export const ProductPage = () => {
 		},
 	];
 
-	// const itemdropdown: MenuProps["items"] = [
-	// 	{
-	// 		label: "ทั้งหมด",
-	// 		key: "1",
-	// 	},
-	// 	{
-	// 		label: "ร่าง",
-	// 		key: "2",
-	// 	},
-	// 	{
-	// 		label: "รออนุมัติ",
-	// 		key: "3",
-	// 	},
-	// 	{
-	// 		label: "รอตอบรับ",
-	// 		key: "4",
-	// 	},
-	// 	{
-	// 		label: "ตอบรับแล้ว",
-	// 		key: "5",
-	// 	},
-	// ];
-
-	const [collapsed, setCollapsed] = useState(false);
-
 	let [status, setStatus] = useState<string>(itemsCurrency[0].label);
 	const setQuoStatus: MenuProps["onClick"] = ({ key }) => {
 		setStatus((status = key));
@@ -170,131 +51,28 @@ export const ProductPage = () => {
 	};
 
 	return (
-		<div>
+		<>
 			<Title
 				title={t("product and service name")}
 				textButton={""}
 				button={false}
 			/>
 			<Typography>{t("information about products and services")}</Typography>
-			<Row style={{ marginTop: "10px" }} wrap={false}>
-				<Input
-					prefix={
-						<SearchOutlined style={{ color: "black", fontSize: "25px" }} />
-					}
-					placeholder="Search Doccument Number"
-					bordered={true}
-					style={{
-						paddingLeft: "10px",
-						fontSize: "16px",
-						color: ThemeColors.lightOrangeColor,
-						border: "2px solid #EFAB3A",
-						height: "50px",
-						width: "87%",
-					}}
-				/>
-				<Button
-					icon={<SearchOutlined style={{ fontSize: "25px" }} />}
-					style={{
-						marginLeft: "10px",
-						width: "10%",
-						height: "50px",
-						backgroundColor: "#EFAB3A",
-						fontSize: "16px",
-						textOverflow: "ellipsis",
-						overflow: "hidden",
-						color: "white",
-					}}
-				>
-					Search
-				</Button>
-			</Row>
-
-			<Row style={{ marginTop: "10px" }} wrap={false}>
-				<Button
-					onClick={() => setCollapsed(!collapsed)}
-					icon={
-						collapsed ? (
-							<ControlOutlined style={{ fontSize: "30px" }} />
-						) : (
-							<ControlOutlined style={{ color: "#EE9437", fontSize: "30px" }} />
-						)
-					}
-					style={{
-						backgroundColor: collapsed ? "#E46F1B" : "#FFFFFF",
-						width: "5%",
-						height: "70px",
-						border: "0px",
-					}}
-				/>
-				<div
-					style={{
-						marginLeft: "15px",
-						width: "15%",
-						height: "70px",
-						backgroundColor: "white",
-						paddingTop: "10px",
-						paddingLeft: "10px",
-						borderRadius: "10px",
-					}}
-				>
-					<Typography
-						style={{
-							fontSize: "16px",
-							textOverflow: "ellipsis",
-							overflow: "hidden",
-						}}
-					>
-						{t("type")}
-					</Typography>
-					<Form.Item>
-						<Dropdown menu={{ items: itemsCurrency, onClick: setQuoStatus }}>
-							<a onClick={(e) => e.preventDefault()}>
-								<Space
-									style={{
-										width: "90%",
-										display: "flex",
-										justifyContent: "space-between",
-										color: ThemeColors.lightOrangeColor,
-										textOverflow: "ellipsis",
-										overflow: "hidden",
-										marginTop: "-17px",
-									}}
-								>
-									{status}
-									<DownOutlined />
-								</Space>
-							</a>
-						</Dropdown>
-					</Form.Item>
-				</div>
-				<Link to="/admin/product/create" style={{ width: "12%" }}>
-					<Button
-						type="primary"
-						icon={<PlusOutlined style={{ fontSize: "25px" }} />}
-						style={{
-							width: "100%",
-							height: "70px",
-							backgroundColor: "#E46F1B",
-							fontSize: "16px",
-							marginLeft: "10px",
-							overflow: "hidden",
-							textOverflow: "ellipsis",
-						}}
-					>
-						{t("add product")}
-					</Button>
-				</Link>
-			</Row>
-			<Row>
-				<Table
-					size="middle"
-					pagination={false}
-					columns={columns}
-					dataSource={product}
-					style={{ marginTop: "15px", textAlign: "center", width: "100%" }}
-				/>
-			</Row>
-		</div>
+			<SearchBox placeHolder={"Search for User"} />
+			<FilterBar
+				onFilterClick={() => setCollapsed(!collapsed)}
+				filterIcon={
+					collapsed ? (
+						<ControlOutlined style={{ fontSize: "30px" }} />
+					) : (
+						<ControlOutlined style={{ color: "#EE9437", fontSize: "30px" }} />
+					)
+				}
+				filterCollapsed={collapsed}
+				role={status}
+				roleMenu={{ items: itemsCurrency, onClick: setQuoStatus }}
+			/>
+			<ProductTable />
+		</>
 	);
 };
