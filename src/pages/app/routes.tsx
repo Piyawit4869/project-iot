9aeeb07611e1984d9b23a65c2f9b39089e285022
annoppanna <annop@studiotwist.co>
2024-05-07 21:54:14 +0700

    fix(structure): structure

--- src/pages/app/routes.tsx
+++ src/pages/app/routes.tsx
@@ -1,10 +1,20 @@
+import { LoginPage } from "./auth";
 import { AppLandingPage } from "./indexpage";
+
 import { routes as planningRoute } from "./planning";
+import { routes as notationRoute } from "./notation";
+import { routes as attendanceRoute } from "./attendance";
 
 export const routes = [
   {
     path: "",
     element: <AppLandingPage />,
   },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
   ...planningRoute,
+  ...notationRoute,
+  ...attendanceRoute,
 ];
