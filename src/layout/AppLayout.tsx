997ac60e5c6cafca79b44e81571c19e2aaf29d20
luxuryheart <pnkhotmail@gmail.com>
2024-07-13 00:27:44 +0700

    fix(api): reconnected api organize in super admin view and fix loading on layout

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,20 +1,7 @@
 import { Layout } from "antd";
-import { useNavigation } from "react-router-dom";
 import { Contents, Headerbar, Sidebar } from ".";
-import React from "react";
 
 export const AppLayout = () => {
-  const [loading, setLoading] = React.useState(true);
-  const { state } = useNavigation();
-
-
-  
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
 
   return (
     <>
@@ -24,8 +11,7 @@ export const AppLayout = () => {
         <Sidebar />
         <Layout>
           <Headerbar />
-
-          <Contents loading={loading || state === "submitting"} />
+          <Contents />
         </Layout>
       </Layout>
     </>
--- src/pages/app/organize/loader.ts
+++ src/pages/app/organize/loader.ts
@@ -1,9 +1,9 @@
+import * as API from "../../../apis";
 
 export async function organizeLoader() {
     try {
-    //   const organize = await API.organize.getAll();
-    //   return { organize: organize.data };
-    return {};
+      const organize = await API.organize.getAll();
+      return { organize: organize.data.items };
     } catch (error) {
       return { error: "error", message: error };
     }
