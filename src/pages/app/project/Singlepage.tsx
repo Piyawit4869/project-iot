5c7f5c98d40f01796676ee7e4b4ec3e59e644572
phuwis <phuwis.tweed@gmail.com>
2024-08-24 23:32:25 +0700

    fix(form): removed old property of dynamic form

--- src/pages/app/project/Singlepage.tsx
+++ src/pages/app/project/Singlepage.tsx
@@ -132,12 +132,9 @@ export const ProjectSingle: React.FC = () => {
                         option={item.option}
                         icon={item.icon}
                         value={item.value}
-                        ruleMessage={item.message}
-                        require={item.require}
                         disabled={item.disabled}
                         checked={item.checked}
                         maxLength={item.maxLength}
-                        validator={item.validator}
                       />
                     );
                   })}
