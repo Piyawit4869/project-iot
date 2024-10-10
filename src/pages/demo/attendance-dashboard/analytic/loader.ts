b793a4729fa3876f99ace181fd9fe15ae73fde0a
annoppanna <annoppanna@gmail.com>
2024-10-11 05:26:17 +0700

    fix(remove-unuse-file): remove-unuse-file

--- src/pages/demo/attendance-dashboard/analytic/loader.ts
+++ src/pages/demo/attendance-dashboard/analytic/loader.ts
@@ -1,12 +1,12 @@
-import { LoaderFunctionArgs } from 'react-router-dom';
-import * as API from '@src/apis';
+// import { LoaderFunctionArgs } from 'react-router-dom';
+// import * as API from '@src/apis';
 
-export async function analyticPageLoader({ request }: LoaderFunctionArgs) {
+export async function analyticPageLoader() {
   try {
-    const atttendances = await API.attendance.getAll();
+    // const atttendances = await API.attendance.getAll();
     //   return { organize: organize.data };
 
-    return { data: atttendances.data };
+    return { data: {} };
   } catch (error) {
     return { status: 'error', message: error };
   }
--- src/pages/demo/attendance-dashboard/appointment/loader.ts
+++ src/pages/demo/attendance-dashboard/appointment/loader.ts
@@ -1,12 +1,13 @@
-import { LoaderFunctionArgs } from 'react-router-dom';
-import * as API from '@src/apis';
+// import { LoaderFunctionArgs } from 'react-router-dom';
+// import * as API from '@src/apis';
 
-export async function appointmentPageLoader({ request }: LoaderFunctionArgs) {
+export async function appointmentPageLoader() {
+  // export async function appointmentPageLoader({ request }: LoaderFunctionArgs) {
   try {
-    const atttendances = await API.attendance.getAll();
+    // const atttendances = await API.attendance.getAll();
     //   return { organize: organize.data };
 
-    return { data: atttendances.data };
+    return { data: {} };
   } catch (error) {
     return { status: 'error', message: error };
   }
