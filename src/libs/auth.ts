b77007ed826229e36650709d46e0bb4548631697
phuwis <phuwisw@gmail.com>
2025-01-21 02:43:53 +0700

    fix:optimize-admin-page

---
+++ src/libs/auth.ts
@@ -0,0 +1,24 @@
+import { getSession, useSession } from 'next-auth/react';
+
+export const getServerSession = async () => {
+  const session = await getSession();
+
+  const user: any = session && session.user;
+
+  const accessToken = user.auth && user.auth.accessToken;
+  const refreshToken = user.auth && user.auth.refreshToken;
+
+  return { accessToken, refreshToken };
+};
+
+export const getClientSession = () => {
+  const { data: session, status } = useSession();
+
+  const user: any = status === 'authenticated' && session && session.user;
+
+  const me = user && user.me ? user.me : null;
+
+  console.log({ me });
+
+  return me;
+};
