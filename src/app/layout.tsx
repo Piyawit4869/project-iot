dba2781b9e26d87861af1c42ce13f97c9013a410
annoppanna <annoppanna@gmail.com>
2025-04-08 15:17:16 +0700

    fix(fixed-query-data-table): fixed-query-data-table

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-/** @format */
-
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import Providers from "./providers";
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,5 +1,3 @@
-/** @format */
-
 "use client";
 
 import * as React from "react";
