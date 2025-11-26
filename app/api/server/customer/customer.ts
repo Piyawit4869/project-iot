7c246e28c3e4df3c4db43f4cf3de7ea479caaf32
pechhyy <pechna212@gmail.com>
2025-11-26 15:56:05 +0700

    fix(fix-ai-reply-setting): fixed ai reply setting on chat room

--- app/api/server/customer/customer.ts
+++ app/api/server/customer/customer.ts
@@ -120,7 +120,7 @@ export const fetchAiReplySettings = async (
 ) => {
   try {
     const res = await ApiConfig.put(
-      `/crud/customers/edit/${id}/ai-reply-settings`,
+      `/crud/chats/rooms/edit/${id}/ai-reply-settings`,
       payload
     );
 
@@ -142,14 +142,13 @@ export const fetchDeleteCustomer = async (id: string) => {
 export const fetchCustomerSummary = async (customerType?: string) => {
   try {
     const res = await ApiConfig.get(`/crud/customers/status-summary`, {
-      params:  {customerType} ,
+      params: { customerType },
     });
     return res.data;
   } catch (error) {
     throw error;
   }
 };
-
 
 export const fetchAllContact = async () => {
   try {
@@ -166,6 +165,18 @@ export const fetchGetAnalyzeCustomer = async (id: string) => {
     const res = await ApiConfig.get(
       `/crud/customers/${id}/summary-chat-message
        `
+    );
+
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchGetAiSettings = async (id: string) => {
+  try {
+    const res = await ApiConfig.get(
+      `/crud/chats/rooms/${id}/ai-reply-settings`
     );
 
     return res.data;
