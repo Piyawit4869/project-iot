006acdcfe4df31b5a16f94d2bacc9a9cfcd67bc9
khai_nong <nongnaphat.nua@gmail.com>
2025-12-05 21:45:56 +0700

    fix(fix-customer-flied-and-disable-btn): fix-customer-flied-and-disable-btn

--- app/components/modules/customer/single-detail-customer.tsx
+++ app/components/modules/customer/single-detail-customer.tsx
@@ -61,6 +61,10 @@ export default function SingDetailleCustomer() {
     .trim();
   const phoneContactState = formUpdate.watch("contacts.0.phone");
   const nameContactState = formUpdate.watch("contacts.0.name");
+  const isAnyFilled =
+    (!!phoneContactState?.trim() && !nameContactState?.trim()) ||
+    (!phoneContactState?.trim() && !!nameContactState?.trim());
+
   const { isDirty } = formUpdate.formState;
 
   useEntityBreadcrumb({
@@ -260,12 +264,7 @@ export default function SingDetailleCustomer() {
                     type="submit"
                     form="customer"
                     loading={isUpdating}
-                    disabled={
-                      isLoading ||
-                      isPending ||
-                      !phoneContactState?.trim() ||
-                      !nameContactState?.trim()
-                    }
+                    disabled={isLoading || isPending || isAnyFilled}
                     className="flex-1  flex items-center gap-1 px-2 py-1 text-xs sm:px-4 sm:py-2 sm:text-sm dark:disabled:bg-transparent dark:disabled:text-white dark:disabled:border-white  dark:disabled:border-1"
                     icon={<Save />}
                     label={<span className="hidden sm:inline">บันทึก</span>}
