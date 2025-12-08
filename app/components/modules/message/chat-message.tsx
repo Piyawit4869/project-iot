580eb36e048463e5550c7ba941e36cc8c00e3752
annoppanna <annoppanna@gmail.com>
2025-12-09 02:24:40 +0700

    fix(refactor-component-chat): refactor-component-chat

--- app/components/modules/message/chat-message.tsx
+++ app/components/modules/message/chat-message.tsx
@@ -1,32 +1,25 @@
-import { Avatar, AvatarFallback } from "~/components/ui/avatar";
 import React, { useRef, useState } from "react";
 import dayjs from "dayjs";
 import _ from "lodash";
 import { v4 as uuidv4 } from "uuid";
 
-import FeatureCard from "~/components/shared/feature-card";
-import { MessagesSquare } from "lucide-react";
-import ChatInput from "./chat-input";
-
 import { socketConfig } from "~/lib/sockets";
-import StatusToolbar from "./status-toolbar";
 import { CustomerChatSkeleton } from "./noData/customer-chat-skeleton";
 import type { ChatRoomSchemaType } from "~/schemas/message/message";
 import { usePaginatedMessagesCursor } from "~/api/client/message/useMessage";
 import { useChat, type Message } from "~/providers/chat/useChat";
-import { formatDateAndTime } from "~/components/shared/global-format";
 
-import LoadingAnimation from "./loading-animation";
-import { MessageRenderer } from "./render-message-content";
-import { MessageMenu } from "./MessageMenu";
 import { useRouteLoaderData } from "react-router";
+import { MessageNoData } from "./chat/MessageNoData";
+import { MessagePreviewImage } from "./chat/MessagePreviewImage";
+import { MessageHeader } from "./chat/MessageHeader";
+import { MessageBody } from "./chat/MessageBody";
 
 export const ChatMessages = ({
   api,
   subId,
   customer,
   selectedRoom,
-  isLineGroup,
   setAutoScroll,
 }: {
   api: string;
@@ -66,7 +59,6 @@ export const ChatMessages = ({
 
   const [showTopLoading, setShowTopLoading] = useState(false);
   const [buttonScrollToBottom, setButtonScrollToBottom] = React.useState(false);
-  const [hasInitialScroll, setHasInitialScroll] = React.useState(false);
 
   const [replyRefMessage, setReplyRefMessage] = useState<string | null>(null);
 
@@ -199,7 +191,6 @@ export const ChatMessages = ({
     setTargetMessageId(messageId);
     setDirection("none");
     setHasScrolledToTarget(false);
-    setHasInitialScroll(false);
   };
 
   const onReply = (msg: any) => {
@@ -276,8 +267,6 @@ export const ChatMessages = ({
     if (!el || !combinedMessages?.length) return;
     requestAnimationFrame(() => {
       el.scrollTop = el.scrollHeight;
-
-      setHasInitialScroll(true);
     });
   }, [!!combinedMessages?.length]);
 
@@ -329,7 +318,6 @@ export const ChatMessages = ({
         });
       }
 
-      // if (targetMessageId) {
       const isBottom =
         el.scrollTop + el.clientHeight >= el.scrollHeight - THRESHOLD;
 
@@ -337,7 +325,6 @@ export const ChatMessages = ({
         setDirection("next");
         fetchNextPage();
       }
-      // }
     };
 
     el.addEventListener("scroll", onScroll);
@@ -397,212 +384,52 @@ export const ChatMessages = ({
   }
 
   if (isNoMessageData) {
-    return (
-      <div className="flex flex-col h-[200px] w-full justify-center items-center gap-12">
-        <h2 className="text-center text-2xl">
-          ยินดีต้อนรับสู่แชท Feature ที่ผนวกร่วมกับ Rome AI
-        </h2>
-        <div className="w-[300px]">
-          <FeatureCard
-            icon={<MessagesSquare className="w-8 h-8 text-blue-500" />}
-            title="แชท sale AI & Support"
-            description="ช่องทางแชทระหว่างฝ่ายขายและลูกค้า พร้อมผนวก AI ช่วยตอบคำถามและสนับสนุนการสนทนาอย่างรวดเร็วและแม่นยำ"
-          />
-        </div>
-      </div>
-    );
+    return <MessageNoData />;
   }
 
   return (
-    <div className="flex flex-col h-[calc(100vh-100px)] bg-white  dark:bg-background">
-      <div className="items-center justify-between gap-4 p-2 border-b bg-white dark:bg-background">
-        <div className="hidden xl:block">
-          <StatusToolbar
-            chatRoomDetail={selectedRoom}
-            total={messagesData?.pages[0]?.meta?.total ?? 0}
-            onSearchClick={handleSearchClick}
-          />
-        </div>
-      </div>
+    <div className="flex flex-col h-[calc(100vh-100px)] bg-white dark:bg-background">
+      <MessageHeader
+        chatRoomDetail={selectedRoom}
+        total={messagesData?.pages[0]?.meta?.total ?? 0}
+        onSearchClick={handleSearchClick}
+      />
 
-      <div className="flex flex-1 flex-col max-h-[calc(100vh-175px)]">
-        <div
-          ref={scrollAreaRef}
-          className="flex h-full flex-col space-y-4 overflow-y-auto px-4 z-0 relative dark:bg-background"
-        >
-          {showTopLoading && (
-            <div className={messageLoadingStyle}>กำลังโหลดข้อความ...</div>
-          )}
-
-          {combinedMessages &&
-            combinedMessages.length > 0 &&
-            _.uniqBy(combinedMessages, "id").map((msg: any, index: number) => {
-              const isBackoffice = msg.platform === "backoffice";
-
-              if (msg.messageLabel === "ROME AI กำลังประมวลผล") return null;
-
-              const isTarget = msg.id === targetMessageId;
-
-              const avatarFallback =
-                msg.imageUrl && !msg.imageUrl.includes("http")
-                  ? `https://ui-avatars.com/api/?name=${encodeURIComponent(
-                      msg.imageUrl
-                    )}`
-                  : msg?.imageUrl;
-
-              const formattedTime = formatDateAndTime(
-                msg.createdAt ? msg.createdAt : msg.timestamp
-              );
-
-              return (
-                <div
-                  key={`${index}-${msg.id}`}
-                  ref={(el) => (messageRefs.current[msg.id] = el) as any}
-                  id={`msg-${msg.id}`}
-                >
-                  {msg && msg?.firstMessageToday && (
-                    <div className="flex items-center justify-center pt-6">
-                      <span className="text-sm text-[12px] text-muted-foreground">
-                        {formattedTime}
-                      </span>
-                    </div>
-                  )}
-                  <div
-                    className={`group flex flex-col ${msg.isLabel ? "" : "max-w-[75%]"} ${
-                      msg.platform !== "line"
-                        ? "items-end ml-auto"
-                        : "items-start mr-auto"
-                    } ${msg.isFirstInGroup ? "pt-5" : "pt-0"}`}
-                  >
-                    {msg.showAvatar && !msg.isLabel && (
-                      <div className="flex items-center gap-2 mb-1">
-                        <Avatar className="w-6 h-6">
-                          <img
-                            src={avatarFallback || "/avatar.png"}
-                            alt="avatar"
-                            className="rounded-full object-cover"
-                          />
-                          <AvatarFallback>
-                            {(msg.sender || msg.recipient || "U")[0]}
-                          </AvatarFallback>
-                        </Avatar>
-                        <span className="text-xs text-muted-foreground font-medium">
-                          {msg.sender || msg.recipient || "Anonymous"}
-                        </span>
-                      </div>
-                    )}
-
-                    {/* <div className="flex flex-row gap-2 items-center"> */}
-                    <MessageRenderer
-                      msg={msg}
-                      isBackoffice={isBackoffice}
-                      setPreviewUrl={setPreviewUrl}
-                      playing={playing}
-                      setPlaying={setPlaying}
-                      currentTime={currentTime}
-                      setCurrentTime={setCurrentTime}
-                      duration={duration}
-                      setDuration={setDuration}
-                      audioRef={audioRef}
-                      togglePlay={togglePlay}
-                    />
-                    {msg.platform === "line" && (
-                      <MessageMenu
-                        msg={msg}
-                        onReply={() => onReply(msg)}
-                        onCopy={() => copyMessage(msg.message)}
-                      />
-                    )}
-                    {/* </div> */}
-
-                    {msg.showTime && !msg.isLabel && (
-                      <span className="text-[10px] text-muted-foreground mt-1 ">
-                        {msg.read && <span>อ่านแล้ว,</span>} {formattedTime}
-                      </span>
-                    )}
-                  </div>
-                </div>
-              );
-            })}
-
-          {lastMessage.messageLabel === "ROME AI กำลังประมวลผล" && (
-            <div className="w-full mt-4 flex justify-end flex-col gap-1 mr-auto items-end">
-              <div className="flex items-center gap-2 mb-1">
-                <Avatar className="w-6 h-6">
-                  <img
-                    src={"https://api.dicebear.com/9.x/glass/svg?seed=rome"}
-                    alt="avatar"
-                    className="rounded-full object-cover"
-                  />
-                  <AvatarFallback>{"U"[0]}</AvatarFallback>
-                </Avatar>
-
-                <span className="text-xs text-muted-foreground font-medium">
-                  ROME AI Assistant
-                </span>
-              </div>
-              <div
-                className={`rounded-xl px-4 py-2 text-sm whitespace-pre-wrap bg-muted text-primary"`}
-              >
-                <LoadingAnimation />
-              </div>
-            </div>
-          )}
-
-          <div ref={bottomRef} />
-          {buttonScrollToBottom && (
-            <button onClick={scrollToBottom} className={seemoreStyle}>
-              ดูข้อความล่าสุด
-            </button>
-          )}
-        </div>
-
-        <ChatInput
-          subId={subId}
-          selectedRoom={selectedRoom}
-          customer={customer}
-          replyRefMessage={replyRefMessage}
-          setReplyRefMessage={setReplyRefMessage}
-        />
-      </div>
+      <MessageBody
+        ref={scrollAreaRef}
+        messageRefs={messageRefs}
+        showTopLoading={showTopLoading}
+        combinedMessages={combinedMessages}
+        bottomRef={bottomRef}
+        audioRef={audioRef}
+        playing={playing}
+        setPlaying={setPlaying}
+        currentTime={currentTime}
+        setCurrentTime={setCurrentTime}
+        duration={duration}
+        setDuration={setDuration}
+        togglePlay={togglePlay}
+        setPreviewUrl={setPreviewUrl}
+        buttonScrollToBottom={buttonScrollToBottom}
+        scrollToBottom={scrollToBottom}
+        onReply={onReply}
+        replyRefMessage={replyRefMessage}
+        setReplyRefMessage={setReplyRefMessage}
+        copyMessage={copyMessage}
+        lastMessage={lastMessage}
+        subId={subId}
+        selectedRoom={selectedRoom}
+        customer={customer}
+      />
 
       {previewUrl && (
-        <div
-          className="fixed inset-0 z-50 bg-black/70 flex items-center justify-center p-4"
-          role="dialog"
-          aria-modal="true"
-          onClick={() => setPreviewUrl("")}
-        >
-          <div
-            className="relative bg-transparent rounded-lg overflow-hidden max-w-5xl max-h-[90vh]"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <img
-              src={previewUrl}
-              alt="preview"
-              className="w-auto h-[90vh] object-contain rounded-lg"
-              width={1200}
-              height={800}
-            />
-          </div>
-        </div>
+        <MessagePreviewImage
+          previewUrl={previewUrl}
+          setPreviewUrl={setPreviewUrl}
+        />
       )}
     </div>
   );
 };
-
-const messageLoadingStyle =
-  "absolute top-4 left-1/2 -translate-x-1/2 z-30 bg-white dark:bg-gray-800 text-xs text-muted-foreground text-center py-2 px-4 rounded-lg shadow-md w-fit";
-
-const seemoreStyle = `
-                    sticky bottom-5 left-1/2 -translate-x-1/2 z-20
-                    bg-white dark:bg-gray-800
-                    text-xs text-muted-foreground text-center
-                    py-2 px-4
-                    rounded-full shadow-lg
-                    w-fit cursor-pointer
-                    hover:bg-gray-100 dark:hover:bg-gray-700
-                    transition-colors duration-200
-                `;
 
 export default ChatMessages;
