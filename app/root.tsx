67efe8660068310bc18ec834a240e48dfcd59a6c
annoppanna <annoppanna@gmail.com>
2025-12-31 02:03:48 +0700

    fix(fix-tag-ga): fix-tag-ga

--- app/root.tsx
+++ app/root.tsx
@@ -54,21 +54,19 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>ROME Platform</title>
 
-        {/* Google Tag Manager */}
-        {/* <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-T724KX5N');
-            `,
+        (function(w,d,s,l,i){w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T724KX5N');
+      `,
           }}
-        /> */}
-        {/* End Google Tag Manager */}
+        />
 
         <Meta />
         <Links />
