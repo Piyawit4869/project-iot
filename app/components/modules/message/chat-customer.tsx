d3c9e2ff1bc486d4ddbfc6a0dacc79c72173aa1b
pechhyy <pechna212@gmail.com>
2026-01-04 01:11:00 +0700

    fix(fix-scroll-and-date): fixed scroll on chat ai and handle new date format

--- app/components/modules/message/chat-customer.tsx
+++ app/components/modules/message/chat-customer.tsx
@@ -1409,13 +1409,6 @@ export default function ChatCustomerInfo({
         onOpenChange={setCheckStatusOpen}
       />
 
-      <AIMessageView
-        open={AIOpen}
-        onOpenChange={setAIOpen}
-        customer={dataFromAI}
-        noSyncBtn={true}
-      />
-
       <AboutCustomer
         open={addCustomerDetail}
         onOpenChange={setAddCustomerDetail}
